f762e6aece2e27d497f36a78f3bc720ac2050c46
Andleeb50 <andleebmuneer22@gmail.com>
2024-07-12 22:18:55 +0530

    Add files via upload

---
+++ q14.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Sequence: ");
+    
+    // Loop from n to 1
+    for (int i = n; i >= 1; --i) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
