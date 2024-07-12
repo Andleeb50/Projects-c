ebf49ea4a369ea8a8373c1afb89b53a6d7c87168
Andleeb50 <andleebmuneer22@gmail.com>
2024-07-12 21:43:18 +0530

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+
+    // Reading two numbers from the keyboard
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    // Comparing the numbers
+    if (num1 > num2) {
+        printf("%d is greater than %d\n", num1, num2);
+    } else if (num1 < num2) {
+        printf("%d is less than %d\n", num1, num2);
+    } else {
+        printf("%d is equal to %d\n", num1, num2);
+    }
+
+    return 0;
+}
+    
