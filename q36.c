5f014e0aaa0146c2dcef84907222acdc5c783e20
Andleeb50 <andleebmuneer22@gmail.com>
2024-07-13 10:44:38 +0530

    Update q36.c

--- q36.c
+++ q36.c
@@ -1,13 +1,14 @@
+//Input an integer array from the user and display the same.
 #include <stdio.h>
 
 int main() {
     char arr[5] = "abid";
     int i = 0;
-    
+
     while(arr[i] != '\0') 
+    {
         printf("%c", arr[i]);
         i++;
     }
-    
-    return 0; 
-}
+
+    }
