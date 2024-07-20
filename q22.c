419420c9c1b9aafaf9e7e57c0b76880801242aa1
Andleeb50 <andleebmuneer22@gmail.com>
2024-07-20 07:00:22 +0530

    q22.c

--- q22.c
+++ q22.c
@@ -1,28 +1,21 @@
-/*Display the Fibonacci series: 0 1 1 2 3 5 8 13 21 ....... (n terms)*/
-
+//display the fibonacci series: 0 1 1 2 3 5 8 13 21...
 #include <stdio.h>
-#include<math.h>
-
-int main() {
-    int a, b, c, n, x, i;
-
-    printf("Enter the number of terms you want in Fibonacci: ");
-
-    scanf("%d", &x);
-
-    n = x;  // Initialize n with the value of x
-    a = 0;
-    b = 1;  // Initial values of the Fibonacci series
-
-    printf("%d %d", a, b);  // Print the first two terms
-
-    for (i = 1; i <= n - 2; i++) {
-        c = a + b;
-        printf(" %d", c); 
-        a = b;
-        b = c;
+int main()
+{
+    int a,b,c,i,n;
+    printf("enter the no. of terms: ");
+    scanf("%d",&n);
+    a=0;
+    b=1;
+    printf("%d  ",a);
+    printf("%d  ",b);
+    for(i=1;i<=n-2;i++)
+    {
+        c=a+b;
+        printf("%d  ",c);
+        a=b;
+        b=c;
+    
     }
-
-    printf("\n");
     return 0;
 }
