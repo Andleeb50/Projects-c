3cb515960793887866090f04a03da1fcda96ce00
Andleeb50 <andleebmuneer22@gmail.com>
2024-07-20 07:28:35 +0530

    q19.c

--- q19.c
+++ q19.c
@@ -2,28 +2,23 @@
 #include <stdio.h>
 
 int main() {
-    int num, i, prime = 1; //prime by default
+    int i, n, flag = 1; 
+    
+    printf("Enter the number to be checked: ");
+    scanf("%d", &n);
 
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    if (num <= 1) {
-        prime = 0; // Numbers less than or equal to 1 are not prime
-    } else {
-        for (i = 2; i <= num / 2; ++i) {
-            if (num % i == 0) {
-                prime = 0; // Found a divisor other than 1 and itself
-                break;
-            }
+    for (i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            flag = 0;
+            break; 
         }
     }
 
-    if (prime == 1) {
-        printf("%d is a prime number.\n", num);
+    if (flag == 1) {
+        printf("The number is prime\n");
     } else {
-        printf("%d is not a prime number.\n", num);
+        printf("The number is not prime\n");
     }
 
     return 0;
 }
- 
